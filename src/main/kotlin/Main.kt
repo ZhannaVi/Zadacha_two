bb68429a1550386d1857cf523f1b46fa62fce17a
ZhannaVi <zhanna08012005@mail.ru>
2023-09-23 12:20:17 +0300

    Доделать правильный счет символов

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,36 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+val oneStr = "AASADDSS"
+    var count = 1
+    var currentChar = oneStr[0]
+    var itogStr=""
+    for(item in 1 .. oneStr.substring(1).length)
+    {
+        if(currentChar==oneStr[item])
+        {
+            count++
+            currentChar=oneStr[item]
+        }
+        else
+        {
+            if(count>1)
+            {
+                itogStr+="$currentChar$count"
+            }
+            else
+            {
+                itogStr+=currentChar
+            }
+            count = 1
+            currentChar=oneStr[item]
+        }
+    }
+    if(count>1)
+    {
+        itogStr+="$currentChar$count"
+    }
+    else
+    {
+        itogStr+=currentChar
+    }
+    println(itogStr)
 }
